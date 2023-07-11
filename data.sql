d4261e0cf3b84f8c15f9093d1793c6317f666cc1
kifle23 <kiflekifle23@gmail.com>
2023-07-11 20:41:17 +0300

    Add column species to animals table
    Insert data to animals table
    
    Insert data to animals table
    
    Create transaction & update/delete/commit/rollbac transaction
    
    Query table

--- data.sql
+++ data.sql
@@ -4,3 +4,12 @@ VALUES
     ('Gabumon', '2018-11-15', true, 8, 2),
     ('Pikachu', '2021-01-07', false, 15.04, 1),
     ('Devimon', '2017-05-12', true, 11, 5);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES 
+    ('Charmander', '2020-02-08', -11.0, false, 0),
+    ('Plantmon', '2021-11-15', -5.7, true, 2),
+    ('Squirtle', '1993-04-02', -12.13, false, 3),
+    ('Angemon', '2005-06-12', -45.0, true, 1),
+    ('Boarmon', '2005-06-07', 20.4, true, 7),
+    ('Blossom', '1998-10-13', 17.0, true, 3),
+    ('Ditto', '2022-05-14', 22.0, true, 4);
